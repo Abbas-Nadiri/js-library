4dbd6a8c8bd2a28e56fac8bc503b2ce5da407c62
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-09-24 06:29:22 +0100

    create addBookToLibrary function and call it in the form button click eventListener

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ function Book(title, author, pages, readStatus) {
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+    /*
     this.info = () => {return `${this.title} by ${this.author}, ${this.pages} pages long, read = ${this.readStatus}`};
+    */
   }
+
+function addBookToLibrary() {
+  const bookTitle = document.getElementById("title");
+  const bookAuthor = document.getElementById("author");
+  const bookPages = document.getElementById("pages");
+  const bookStatus = document.querySelector("input[name='read-status']:checked");
+  if(bookTitle && bookAuthor && bookPages && bookStatus) {
+    const newBook = new Book(bookTitle.value, bookAuthor.value, Number(bookPages.value), bookStatus.value);
+    myLibrary.push(newBook);
+  } else {
+    alert("Please fill in all sections before adding to library.")
+  }
+}
+
 
 const body = document.querySelector("body");
 const container = document.querySelector(".library");
@@ -14,7 +30,20 @@ const button = document.querySelector(".addBook");
 
 button.addEventListener("click", () => {
   event.preventDefault();
+  addBookToLibrary();
+});
+
+/*
   const div = document.createElement("div");
   div.textContent = "new book";
   container.appendChild(div);
-});
+*/
+
+/* 
+  const bookStatus = document.querySelector("input[name='read-status']:checked");
+  if (bookStatus) {
+    console.log("Read status:", bookStatus.value);
+  } else {
+    console.log("No read status selected");
+  }
+*/
