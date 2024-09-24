499204d9eed37fc62e35d6f0dceb9f35833712fb
ahnadiri <108028111+Abbas-Nadiri@users.noreply.github.com>
2024-09-24 10:51:02 +0100

    implement remove book button and fix createBook functionality

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
+const library = document.querySelector(".library");
+const button = document.querySelector(".addBook");
+
+function Book(title, author, pages, readStatus, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
-    /*
-    this.info = () => {return `${this.title} by ${this.author}, ${this.pages} pages long, read = ${this.readStatus}`};
-    */
+    this.id = id;
+  }
+
+  function createBook(book) {
+    const div = document.createElement("div");
+    const bookTitle = document.createElement("p");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const readDiv = document.createElement("div");
+    const readStatusLabel = document.createElement("p");
+    const readStatus = document.createElement("button");
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    bookTitle.textContent = `Title: ${book.title}`;
+    bookAuthor.textContent = `Author: ${book.author}`;
+    bookPages.textContent = `Pages: ${book.pages}`;
+    readStatusLabel.textContent = "Read: "
+    readStatus.textContent = book.readStatus;
+    readDiv.append(readStatusLabel, readStatus);
+    div.append(bookTitle, bookAuthor, bookPages, readDiv, removeButton);
+    library.append(div);
+    
+    removeButton.addEventListener("click", () => {
+      div.remove();
+      myLibrary.splice(book.id, 1);
+      myLibrary.forEach(book => book.id = myLibrary.indexOf(book));
+    })
   }
 
 function addBookToLibrary() {
@@ -15,35 +42,17 @@ function addBookToLibrary() {
   const bookAuthor = document.getElementById("author");
   const bookPages = document.getElementById("pages");
   const bookStatus = document.querySelector("input[name='read-status']:checked");
-  if(bookTitle && bookAuthor && bookPages && bookStatus) {
-    const newBook = new Book(bookTitle.value, bookAuthor.value, Number(bookPages.value), bookStatus.value);
+  if(bookTitle.value && bookAuthor.value && bookPages.value && bookStatus.value) {
+    const newBook = new Book(bookTitle.value, bookAuthor.value, Number(bookPages.value), bookStatus.value, null);
     myLibrary.push(newBook);
+    newBook.id = myLibrary.indexOf(newBook);
+    createBook(newBook);
   } else {
     alert("Please fill in all sections before adding to library.")
   }
 }
 
-
-const body = document.querySelector("body");
-const container = document.querySelector(".library");
-const button = document.querySelector(".addBook");
-
 button.addEventListener("click", () => {
   event.preventDefault();
   addBookToLibrary();
 });
-
-/*
-  const div = document.createElement("div");
-  div.textContent = "new book";
-  container.appendChild(div);
-*/
-
-/* 
-  const bookStatus = document.querySelector("input[name='read-status']:checked");
-  if (bookStatus) {
-    console.log("Read status:", bookStatus.value);
-  } else {
-    console.log("No read status selected");
-  }
-*/
